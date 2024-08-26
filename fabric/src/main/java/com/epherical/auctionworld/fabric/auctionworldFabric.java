16d7431801de1eb347ee22c3b3ef4e5cb780614e
panda <pandaismyname1@gmail.com>
2024-08-26 15:36:38 +0300

    Fixed Fabric loading

--- fabric/src/main/java/com/epherical/auctionworld/fabric/auctionworldFabric.java
+++ fabric/src/main/java/com/epherical/auctionworld/fabric/auctionworldFabric.java
@@ -1,11 +1,12 @@
 package com.epherical.auctionworld.fabric;
 
 import com.epherical.auctionworld.AuctionTheWorldAbstract;
-import com.epherical.auctionworld.Events;
 import com.epherical.epherolib.CommonPlatform;
 import com.epherical.epherolib.FabricPlatform;
 import com.epherical.epherolib.networking.FabricNetworking;
+import net.fabricmc.api.EnvType;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.loader.api.FabricLoader;
 
 public final class auctionworldFabric implements ModInitializer {
 
@@ -13,7 +14,7 @@ public final class auctionworldFabric implements ModInitializer {
 
     @Override
     public void onInitialize() {
-        var networking = new FabricNetworking(AuctionTheWorldAbstract.MOD_CHANNEL, true);
+        var networking = new FabricNetworking(AuctionTheWorldAbstract.MOD_CHANNEL,  FabricLoader.getInstance().getEnvironmentType() == EnvType.CLIENT);
         abs = new AuctionTheWorldAbstract(networking);
         CommonPlatform.create(new FabricPlatform());
     }
